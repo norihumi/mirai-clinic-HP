85f254220316e9aaaf234b1c03c367b1c0ee0d3e
norihumi <norihumiy03@gmail.com>
2026-08-28 19:19:13 +0900

    08th : フッター

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // 要素の取得
 const hamburger = document.querySelector('.js-hamburger');
 const nav = document.querySelector('.js-nav');
+const navLinks = document.querySelectorAll('.header__link');
 
 // ハンバーガーボタンがクリックされた時の処理
 hamburger.addEventListener('click', () => {
